bebcb7de02002f47485dd8fc84c42d3e2b3e8a11
Guillaume Claret <guillaume@ouicar.fr>
2015-12-21 11:32:37 +0100

    A run function just for the computations

--- src/computation/lib.js
+++ src/computation/lib.js
@@ -1,5 +1,22 @@
 import C from './index';
 
+export function run(computation, runCommand) {
+  switch (computation.type) {
+    case 'return':
+      return computation;
+    case 'call':
+      return runCommand(computation.command);
+    case 'then':
+      const { x, f } = computation;
+      return C.then(run(x, runCommand), result => run(f(result), runCommand));
+    case 'join':
+      const { x, y } = computation;
+      return C.join(run(x, runCommand), run(y, runCommand));
+    default:
+      console.log('unknown computation type', computation);
+  }
+}
+
 export function iterPar(array, f) {
   const [x, ...xs] = array;
   if (x === undefined) {
